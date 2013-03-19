85103b07637a0c91eab9cd58327c58c8fbab0b1c
Julian Mueller <jmueller@jm-designer.de>
2013-03-19 23:03:30 +0100

    renamed .php to .html and replaced php

--- Seapal_php/server/start_server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#
-# Dominic Eschbach 
-# 30.11.2012
-
-/Applications/MAMP/bin/php/php5.4.4/bin/php ./server.php
--- Seapal_php/site/_include/header_app.php
+++
@@ -1,8 +0,0 @@
-	
-	<!-- Additional Styles -->
-	<link href="../css/app/map.css" rel="stylesheet" type="text/css"/>
-	<link href="../css/app/contextMenu.css" rel="stylesheet" type="text/css"/>
-    
-    <!-- Additional Java-Script -->
-    <script src="../js/jquery/jquery-ui.js" type="text/javascript"></script>
-    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBCnaCuuuXCzfGbzbXN699vudQ4fWTNmiY&libraries=geometry&sensor=false"></script>                                
